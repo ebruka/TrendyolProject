923d5e74c3cd3faa7bcfb7e585f7e5b90d36ea2f
ebru.kaya <ebru.kaya@enuygun.com>
2022-06-11 00:50:36 +0300

    feat:testlere description eklendi

--- src/main/java/com/trendyolpages/service/trendyolServices.java
+++ src/main/java/com/trendyolpages/service/trendyolServices.java
@@ -1,6 +1,7 @@
 package com.trendyolpages.service;
 
 import com.trendyolbase.basepages.ServicesBase;
+import com.trendyolbase.data.GetData;
 import com.trendyolpages.requestpojo.BookRequestPojo;
 import com.google.gson.Gson;
 import io.restassured.response.Response;
@@ -8,20 +9,20 @@ import io.restassured.response.Response;
 public class trendyolServices extends ServicesBase {
 
     BookRequestPojo request=new BookRequestPojo();
-    String serviceUrl="https://simple-books-api.glitch.me";
     private  static final String bearerToken="ccc1720956c8930f80cbbe6c3635e9fe568372d2f39284d4afd597b047f751b2";
     
     public trendyolServices getApiStatus() {
-        getRequest(serviceUrl, "", 200);
+        getRequest(GetData.Url.SERVICES_URL.toString(), "", 200);
         return this;
+
     }
     public trendyolServices getAllBook() {
-        getRequest(serviceUrl, "/books", 200);
+        getRequest(GetData.Url.SERVICES_URL.toString(), "/books", 200);
         return this;
     }
 
     public trendyolServices getSingleBook(String bookId) {
-       Response response= getRequest(serviceUrl, "/books/"+bookId+"", 200);
+       Response response= getRequest(GetData.Url.SERVICES_URL.toString(), "/books/"+bookId+"", 200);
        String bookIdResponse=response.jsonPath().getString("id");
        checkExpectedValue(bookIdResponse.equals(bookId),"Aranan book id değeri bulundu..." + "Beklenen değer:" + bookId + "Servisten dönen değer:" + bookIdResponse,"Hata! Beklenen değer gelmedi"+ "Aranan book id:" + bookId);
         return this;
@@ -32,7 +33,7 @@ public class trendyolServices extends ServicesBase {
 
         request.setBookId(3);
         String requestBody = new Gson().toJson(request);
-        postWithTokenRequest(bearerToken,serviceUrl, "/orders",requestBody, 201).asString();
+        postWithTokenRequest(bearerToken,GetData.Url.SERVICES_URL.toString(), "/orders",requestBody, 201).asString();
         return this;
     }
 
@@ -41,7 +42,7 @@ public class trendyolServices extends ServicesBase {
 
         request.setBookId(3);
         String requestBody = new Gson().toJson(request);
-        postWithTokenRequest(bearerToken,serviceUrl, "/orders",requestBody, 201).asString();
+        postWithTokenRequest(bearerToken,GetData.Url.SERVICES_URL.toString(), "/orders",requestBody, 201).asString();
         return this;
     }
 
